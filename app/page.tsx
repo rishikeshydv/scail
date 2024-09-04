e96b6e32454dfdeec2b9b42c465c51472010d369
yaman1337 <yaman.sarabariya@gmail.com>
2024-09-05 00:52:58 +0545

    update footer design

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import { Navbar } from "@/components/navigation";
 import { Button } from "antd";
 import { Check, MoveRight, FileText, MapPinHouse } from "lucide-react";
 import { HistoryReportCard, PropertyCard } from "@/components/card";
-import HeroProject60Image from "@/assets/images/hero-60-project.png";
+import HeroProject100Image from "@/assets/images/hero-100-project.png";
 import HeroHomeImage from "@/assets/images/hero-house.png";
 
 export default function Home() {
@@ -16,15 +16,14 @@ export default function Home() {
           <div className="flex-[0.5] flex flex-col">
             <div className="font-semibold text-[180px] mt-[-50px] flex justify-end">
               <span className="text-[#0874DE]">+</span>
-              <span>Home</span>
+              <span>Property</span>
             </div>
             <div className="font-semibold text-[180px] mt-[-130px] flex justify-end">
-              <span>Upgrade</span>
+              <span>Reports</span>
             </div>
             <div className="font-normal text-[25px] text-[#808080]">
-            Empower your homeownership journey with accurate, reliable reports.
-            Discover the full potential of your home.
-            Your home's story, revealed through expert insights.
+              Discover homes for sale in your city and get detailed property
+              reports
             </div>
             <div className="flex gap-x-4 py-10">
               <Button
@@ -80,7 +79,7 @@ export default function Home() {
               icon={<MoveRight width={15} />}
               iconPosition="end"
             >
-              View All Properties
+              View Homes
             </Button>
           </div>
         </div>
@@ -117,7 +116,7 @@ export default function Home() {
       </section>
       <section className="min-h-[80vh] w-[100vw] overflow-hidden pt-28 pb-32 bg-[#F5F5F5] flex px-52 gap-x-14">
         <div className="flex-[0.5]">
-          <Image src={HeroProject60Image} alt="hero image" height={700} />
+          <Image src={HeroProject100Image} alt="hero image" height={700} />
         </div>
 
         <div className="flex-[0.5] px-4">
@@ -127,7 +126,7 @@ export default function Home() {
           <div className="text-[80px]">
             <span className="font-normal">We help you find </span>
             <span className="font-bold flex flex-col mt-[-30px]">
-              your new place.
+              your new home.
               <span
                 style={{
                   borderBottom: "6px solid rgba(8, 116, 222, 0.5)",
@@ -139,7 +138,7 @@ export default function Home() {
 
           <div className="font-normal text-[25px] text-[#808080] py-10">
             Online property marketplace to buy, sell, and rent residential and
-            commercial properties. Used by millions of renters to find property.
+            commercial properties.
           </div>
 
           <div className="flex flex-col gap-y-3">
@@ -147,16 +146,14 @@ export default function Home() {
               <div className="h-[50px] w-[50px] rounded-[50%] bg-[#E7E5E5] flex items-center justify-center font-medium">
                 <Check size={24} color="#0874DE" />
               </div>
-              <div className="text-[25px]">
-                More than 10 years of experience.
-              </div>
+              <div className="text-[25px]">Detailed property reports</div>
             </div>
             <div className="flex items-center gap-x-3 py-4">
               <div className="h-[50px] w-[50px] rounded-[50%] bg-[#E7E5E5] flex items-center justify-center font-medium">
                 <Check size={24} color="#0874DE" />
               </div>
               <div className="text-[25px]">
-                10,000+ people trusting our agency.
+                Discover the best deals in your city
               </div>
             </div>
           </div>
@@ -169,7 +166,7 @@ export default function Home() {
               icon={<MoveRight width={15} />}
               iconPosition="end"
             >
-              ABOUT THE COMPANY
+              ABOUT US
             </Button>
           </div>
         </div>
@@ -197,7 +194,7 @@ export default function Home() {
               icon={<MoveRight width={15} />}
               iconPosition="end"
             >
-              VIEW ALL PROPERTY
+              GET A HOME REPORT
             </Button>
           </div>
         </div>
@@ -205,27 +202,24 @@ export default function Home() {
         <div className="grid grid-cols-2 w-full gap-5 py-28 px-20">
           <HistoryReportCard
             position={1}
-            title="Find your perfect place."
+            title="Find the perfect place."
             description="Browse through a wide range of 
 properties to find your match"
           />
           <HistoryReportCard
             position={2}
-            title="Schedule survey"
-            description="Browse through a wide range of 
-properties to find your match"
+            title="Enter the address"
+            description="Enter the address in the Reports page"
           />
           <HistoryReportCard
             position={3}
-            title="Make an offer"
-            description="Browse through a wide range of 
-properties to find your match"
+            title="Make your payment"
+            description="Select payment method and complete your payment"
           />
           <HistoryReportCard
             position={4}
-            title="Make your payment"
-            description="Browse through a wide range of 
-properties to find your match"
+            title="Your Prop Fax Report!"
+            description="Get access to your detailed Prop fax report"
           />
         </div>
       </section>
